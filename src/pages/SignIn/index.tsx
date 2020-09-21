6ffc5a29b402514f2e01294a24f457aefc0e7e9a
KevinRichard <kevinrichard@MacBook-Pro.local>
2020-09-21 11:16:45 -0300

    CreateToastAndAnimationToasts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -8,7 +8,8 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 import logo from '../../assets/logo.svg';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationsErrors';
 
 import { Container, Content, Background } from './styles';
@@ -22,6 +23,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -38,17 +40,24 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        signIn({
+        await signIn({
           email: data.email,
           password: data.password,
         });
       } catch (err) {
-        const errors = getValidationErrors(err);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors);
+          formRef.current?.setErrors(errors);
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação.',
+          description: 'Ocorreu um erro ao fazer login, cheque as credencias.',
+        });
       }
     },
-    [signIn],
+    [signIn, addToast],
   );
 
   return (
